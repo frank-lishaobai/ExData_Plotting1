0f3a506ee0a675c51c62c7303505c7a169770f5d
LI Shaobai <frankbluemoon29@icloud.com>
2020-05-29 19:06:37 +0800

    Add files via upload

---
+++ plot3.r
@@ -0,0 +1,10 @@
+EPC<-read.table('./household_power_consumption.txt',header = T,sep = ';')
+EPC_2day<-EPC[which(EPC$Date %in% c('1/2/2007','2/2/2007')),]
+EPC_2day[,3:8]<-sapply(EPC_2day[,3:8],as.numeric)
+with(EPC_2day,plot(Sub_metering_1,type = 'l',ylab='Energy sub metering',xlab = '',xaxt='n'))
+with(EPC_2day,lines(Sub_metering_2,col='red'))
+with(EPC_2day,lines(Sub_metering_3,col='blue'))
+axis(1,c(0,length(which(EPC_2day$Date=='1/2/2007')),length(EPC_2day$Date)),c('Thu','Fri','Sat'))
+legend('topright',pch = '-',col=c('black','red','blue'),legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),cex = 1.5)
+dev.copy(png,file='plot3.png')
+dev.off()
